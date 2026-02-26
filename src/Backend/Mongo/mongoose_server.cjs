3a062760c9c36babc089e6098b94cedd92d2b649
MartinYordanov374 <martinyordanov284@gmail.com>
2026-02-26 20:30:38 +0200

    Dockerized core backend functionality

--- src/Backend/Mongo/mongoose_server.cjs
+++ src/Backend/Mongo/mongoose_server.cjs
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 //TODO: Update the configurations to correspond to the dockerized when dockerizing
 const connectToMongoDB = async() => {
     try{
-        await mongoose.connect('mongodb://localhost:27017/IoT')
+        await mongoose.connect('mongodb://mongo_container:27017/IoT')
         .then((res) => {
             console.log('connection succeeded')
         })
