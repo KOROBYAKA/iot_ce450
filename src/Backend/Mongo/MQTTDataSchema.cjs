9738c6270c790628be30a1057b961b6843743c2c
MartinYordanov374 <martinyordanov284@gmail.com>
2026-02-27 13:31:09 +0200

    Update mongo schema to reflect on the incoming data

--- src/Backend/Mongo/MQTTDataSchema.cjs
+++ src/Backend/Mongo/MQTTDataSchema.cjs
@@ -5,7 +5,10 @@ const MQTTDataSchema = new mongoose.Schema({
     // TODO: The format of the buffer data is to be clarified
     // TODO: Figure out of we want to store everything seperately, 
     // i.e. is every MQTT request to the backend a seperate entry in the DB or not
-    bufferData: {type: String, required: true}
+    humidity: {type: Number, required: true},
+    temperature: {type: Number, required: true},
+    micLevel: {type: Number, required: true},
+
 })
 
 const DataModel = mongoose.model('DataModel', MQTTDataSchema);
