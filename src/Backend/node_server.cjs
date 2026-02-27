65d19a48b4eaafb6f19b67543062f5d73eb18181
MartinYordanov374 <martinyordanov284@gmail.com>
2026-02-27 17:59:55 +0200

    The only backend endpoint now returns the price for the beverage by the latest(by date time) record in the database. For that purpose a pseudoformula was invented.

--- src/Backend/node_server.cjs
+++ src/Backend/node_server.cjs
@@ -8,13 +8,38 @@ const connectToMongoDB = require('./Mongo/mongoose_server.cjs')
 
 const MQTTTopic = 'SendDataToBackend'
 
-app.get('/database/fetch', async (req,res) => {
+app.get('/database/fetchLatestBeveragePrice', async (req,res) => {
     try{
-        const result = await DataModel.find()
-        res.status(200).send({msg: 'Getting MongoDB database data', data: result})
+        const result =  await DataModel.findOne({}).sort({timestamp: -1})
+        const {humidity, temperature, micLevel} = result;
+        // assuming that the base price for coke zero at 10 deg celsius is 2.5 eur, 
+        // then the ratio of eur/degree celsius is 0.25 
+        // Adjusting for the humidity into the final calculation, I divide this by 2 => this ratio is 0.125
+
+        // assuming also that the humidity at the aforementioned temperature is 50 and humidity ranges from 0 to 100, 
+        // then the ratio price to humidity is 0.5. 
+        // Adjusting for the temp, this ratio is also divided by 2 => it is 0.25 
+
+        // I haven't included miclevel in the calculations because it is still unclear how it is going to be used
+
+        // The formula for the price of the beverage is thus Temperature*0.125 + Humidity*0.25
+        // With the aforementioned considerations, the price should be 2.5.
+        const basePrice = 2.5;
+        const baseTemperature = 10; 
+        const baseHumidity = 50;
+        const temperatureCoefficient = (basePrice/baseTemperature) / 2
+        const humidityCoefficient = (basePrice/baseHumidity) / 2
+
+        let price = temperatureCoefficient*temperature + humidityCoefficient*humidity
+        if(price <= 2.5)
+        {
+            price = 2.5
+        }
+        res.status(200).send({msg: 'Getting MongoDB database data', beverageData: {price, temperature, humidity, micLevel}})
     }
     catch(err)
     {
+        console.log(err)
         res.status(500).send({msg: 'something went wrong', err})
     }
 
