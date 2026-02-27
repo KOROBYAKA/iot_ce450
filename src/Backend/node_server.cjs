3fd2a95735c69d218d1f44b7ff5d1410d0dfbce5
MartinYordanov374 <martinyordanov284@gmail.com>
2026-02-27 14:49:50 +0200

    Remove obsolete code from the node server

--- src/Backend/node_server.cjs
+++ src/Backend/node_server.cjs
@@ -10,8 +10,6 @@ const MQTTTopic = 'SendDataToBackend'
 
 app.get('/database/fetch', async (req,res) => {
     try{
-        // TODO: Figure out how the fetching should be done.
-        // Is everything stored into a single schema property or is every entry seperate?
         const result = await DataModel.find()
         res.status(200).send({msg: 'Getting MongoDB database data', data: result})
     }
@@ -20,19 +18,6 @@ app.get('/database/fetch', async (req,res) => {
         res.status(500).send({msg: 'something went wrong', err})
     }
 
-})
-
-app.post('/database/insert', async (req,res) => {
-    try{
-        //TODO: Fix the bufferdata placeholder once the incoming format is known
-        const newModelData = new DataModel({timestamp: new Date(), bufferData: 'test'})
-        await newModelData.save()
-        res.status(200).send('Insert MongoDB database data')
-    }
-    catch(err)
-    {
-        res.status(500).send({msg: 'something went wrong', err})
-    }
 })
 
 app.listen(DEV_PORT, async () => {
@@ -52,7 +37,6 @@ app.listen(DEV_PORT, async () => {
             const receivedPayload = JSON.parse(payload);
             const { humidity, temperature, micLevel } = receivedPayload;
 
-            console.log(humidity, temperature, micLevel)
             const newModelData = new DataModel({timestamp: new Date(), humidity, temperature, micLevel})
             await newModelData.save()
 
