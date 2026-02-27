aef9ed5ce0135c9a7e463d2c7925e1b0dcae630c
MartinYordanov374 <martinyordanov284@gmail.com>
2026-02-27 19:37:36 +0200

    The frontend now fetches the mqtt data from the backend

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,13 +1,14 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './Styles.scss'
 import Axios from 'axios'
 
 const Product = () => {
+ const [beverageData, setBeverageData] = useState([])
  useEffect(() => {
         const fetchData = async () => {
             try {
                 const res = await Axios.get('http://localhost:3000/database/fetchLatestBeveragePrice');
-                console.log(res);
+                setBeverageData(res.data.beverageData);
             } catch (err) {
                 console.log(err);
             }
@@ -22,7 +23,9 @@ const Product = () => {
         className='Image'
         src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi5.walmartimages.com%2Fasr%2Febe17684-42d0-4eb6-b7e7-6a31802118f6.982edd12a4b11fc73d7a645bb87f70d7.jpeg&f=1&nofb=1&ipt=674d58379ba5afdcc56385a5878df4f7f6f637071bb0892978a69bcd5add69ef' />
         <p className='Name'>Coke Zero</p>
-        <p className='Price'>Price: </p>
+        <p className='Price'>Current price: {beverageData.price} eur </p>
+        <p className='Price'>Current temperature: {beverageData.temperature}°C </p>
+        <p className='Price'>Current humidity: {beverageData.humidity}% </p>
     </div>
   )
 }
